cbe25ebed7303ae64b15451df7b613d790791b07
James Ward <james@notjam.es>
2015-01-31 21:28:49 -0500

    rewrite ChannelsController to load more efficiently
    this uses Q to allow an easy load of both stream and channel data
    simultaneously and then only updates the scope once

--- src/scripts/app/controllers/channels.js
+++ src/scripts/app/controllers/channels.js
@@ -2,50 +2,52 @@ module.exports =  [
 	'$scope',
 	'$routeParams',
 	'$twitch',
-	function ($scope, $routeParams, $twitch) {
+	'$q',
+	function ($scope, $routeParams, $twitch, $q) {
 		$scope.channels = {};
 
-		var channels;
+		var loadChannel = function(channel) {
+			if (!channel || channel.length === 0) {
+				// Channels must have a length.
+				return;
+			}
+
+			// Fetch the channel and stream data in tandem since they
+			// both key off of the channel identifier anyway
+			var fetchChannelData = 	$twitch.channel(channel);
+			var fetchStreamData = $twitch.stream(channel);
+
+			$q.all([
+				$twitch.channel(channel),
+				$twitch.stream(channel)
+			])
+			.then(function (result) {
+				if (!result || result.length != 2) {
+					// For some reason these results came back weird..?
+					return;
+				}
+
+				if (result[0].error) {
+					// Error fetching channel data, abort!
+					// This channel probably just doesn't exist.
+					return;
+				}
+
+				var channelData = result[0].data;
+
+				if (result[1].data) {
+					// If the stream data doesn't load we don't need to fret.
+					channelData.stream = result[1].data;
+				}
+
+				// Put the channel data into the scope
+				$scope.channels[channel] = channelData;
+			});
+
+		};
 
 		if ($routeParams.channels) {
-			channels = $routeParams.channels.split('/');
-		} else {
-			channels = [];
+			$routeParams.channels.split('/').map(loadChannel);
 		}
-
-		// Filter empty strings
-		channels = channels.filter(
-			function (e) {
-				return e && e.length != 0;
-			}
-		);
-
-		channels.map(function(channel) {
-			$twitch
-				.channel(channel)
-				.success(function(data) {
-					if (data.error || !data) {
-						return;
-					}
-
-					$scope.channels[channel] = {};
-
-					for (var key in data) {
-						if (data.hasOwnProperty(key)) {
-							$scope.channels[channel][key] = data[key];
-						}
-					}
-
-					$twitch.stream(channel)
-						.success(function(data) {
-							if (data.error) {
-								return;
-							}
-
-							$scope.channels[channel].stream = data.stream;
-						});
-				});
-
-		});
 	}
 ];
